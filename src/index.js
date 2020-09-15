717f1b185be808854bff73e8996658a7a9502d49
kimkhyun <okkihyun@bighitcorp.com>
2020-09-15 23:53:09 +0900

    Print clicked number
    - Make calculator (Markdown)
    - Creating a counter expressed as a decimal number

--- src/index.js
+++ src/index.js
@@ -20,10 +20,40 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render(count = [0]) {
+  const handleNumber = (currNumber) => {
+    if (count.length === 1 && count[0] === 0) {
+      render([currNumber]);
+    }
+    count.splice(0, 1, count[0] * 10 + currNumber);
+    render(count);
+  };
+
+  const handleOperator = (operator) => {
+    console.log('operator::: ', operator);
+  };
+
+  const adder = (accumulator, currentValue) => accumulator + currentValue;
+  const subtractor = (accumulator, currentValue) => accumulator - currentValue;
+  const multiplier = (accumulator, currentValue) => accumulator * currentValue;
+  const divider = (accumulator, currentValue) => accumulator / currentValue;
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <p>{count}</p>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((value) => (
+        <button type="button" onClick={() => handleNumber(value)}>
+          {value}
+        </button>
+      ))}
+      <p>
+        {['+', '-', '*', '/', '='].map((operator) => (
+          <button type="button" onClick={() => handleOperator(operator)}>
+            {operator}
+          </button>
+        ))}
+      </p>
     </div>
   );
 
